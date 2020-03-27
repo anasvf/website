505d087e23168fe9c862f4680080ecc01a21fbd1
anasvf <62753383+anasvf@users.noreply.github.com>
2020-03-27 19:34:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+alert("Hello!, I don't know what i've made but still if you like it please give this code a like");
+
+$(document).ready(function(){
+    $(".items").hide();
+$(".hamburger").click(function(){
+  $(".items").fadeToggle(300)
+  })
+});
